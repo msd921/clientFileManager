24e8d4bc0af55793c6363b6f038e7f5b52810019
msd921 <126858364+msd921@users.noreply.github.com>
2024-11-19 22:34:03 +0300

    starup project

---
+++ src/clientFileManager.cpp
@@ -0,0 +1,12 @@
+﻿// clientFileManager.cpp : Defines the entry point for the application.
+//
+
+#include "clientFileManager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
